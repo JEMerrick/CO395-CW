30946583207bcda6f9ec9429e3ff08548ab098c4
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-10-19 14:01:02 +0100

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -2,3 +2,28 @@ import numpy as np
 
 all = np.loadtxt("WIFI.db/clean_dataset.txt")
 print(all)
+
+source_1 = []
+source_2 = []
+source_3 = []
+source_4 = []
+source_5 = []
+source_6 = []
+source_7 = []
+room_no = []
+
+for datapoint in all:
+    source_1.append(datapoint[0])
+    source_2.append(datapoint[1])
+    source_3.append(datapoint[2])
+    source_4.append(datapoint[3])
+    source_5.append(datapoint[4])
+    source_6.append(datapoint[5])
+    source_7.append(datapoint[6])
+    room_no.append(datapoint[7])
+
+for signal in source_1:
+    print(signal)
+
+for signal in room_no:
+    print(signal)
