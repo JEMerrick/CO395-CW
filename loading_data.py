6b592f622893e037ed580561d5a6cc6140abe658
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-10-19 13:44:29 +0100

    load data

---
+++ loading_data.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+all = np.loadtxt("WIFI.db/clean_dataset.txt")
+print(all)
--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib as mat
-import torch as tor
-
-print(np)
-print(mat)
-print(tor)
