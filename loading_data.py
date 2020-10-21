0f01991c3c89aaad61014b1b67ea1652beb8847c
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-10-21 12:51:39 +0100

    Update loading_data.py

--- loading_data.py
+++ loading_data.py
@@ -25,48 +25,32 @@ fold_8 = all_data[int(7*decile):int(8*decile)]
 fold_9 = all_data[int(8*decile):int(9*decile)]
 fold_10 = all_data[int(9*decile):]
 
+def entropy(array):
+    rooms = []
+    for row in array:
+        rooms.append(row[7])
+    value,label_occurrences = np.unique(rooms, return_counts=True)
+    hits = label_occurrences / len(array)
+    entropy = -(hits * np.log2(hits)).sum()
+    return entropy
+
 #for decision tree
-source_1 = []
-source_2 = []
-source_3 = []
-source_4 = []
-source_5 = []
-source_6 = []
-source_7 = []
-room_no = [] #LABEL
-
-for row in training:
-    source_1.append(row[0]) #all[row][0]
-    source_2.append(row[1])
-    source_3.append(row[2])
-    source_4.append(row[3])
-    source_5.append(row[4])
-    source_6.append(row[5])
-    source_7.append(row[6])
-    room_no.append(row[7])
-
-value,label_occurrences = np.unique(room_no, return_counts=True)
-hits = label_occurrences / len(training)
-entropy_all = -(hits * np.log2(hits)).sum()
+entropy_all = entropy(training)
 print("S(all): " + str(entropy_all))
 
 left = []
 right = []
 for row in training:
-    if(row[0] < -50):
+    if(row[0] < -50): #I AM TESTING ATTRIBUTE SOURCE_1 < -50
         left.append(row)
     else:
         right.append(row)
 
-value,label_occurrences = np.unique(room_no, return_counts=True)
-hits = label_occurrences / len(left)
-entropy_left = -(hits * np.log2(hits)).sum()
+entropy_left = entropy(left)
 print("S(left): " + str(entropy_left))
 
-value,label_occurrences = np.unique(room_no, return_counts=True)
-hits = label_occurrences / len(right)
-entropy_right = -(hits * np.log2(hits)).sum()
-print("S(right): " + str(entropy_right))
+entropy_right = entropy(right)
+print("S(left): " + str(entropy_right))
 
 remainder = (len(left)/(len(left)+len(right)))*entropy_left +(len(right)/(len(left)+len(right)))*entropy_right
 information_gain = entropy_all - remainder
