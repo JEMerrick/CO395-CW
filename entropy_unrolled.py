ea58a916f39366c9283b29caca984d4a83e0e7e2
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-10-21 13:51:26 +0100

    ok

--- entropy_unrolled.py
+++ entropy_unrolled.py
@@ -46,22 +46,3 @@ remainder = (len(left)/(len(left)+len(right)))*entropy_left +(len(right)/(len(le
 information_gain = entropy_all - remainder
 
 print("Information Gain: " + str(information_gain))
-
-
-
-
-
-
-
-
-#Validation
-fold_1 = all_data[:int(decile)]
-fold_2 = all_data[int(decile):int(2*decile)]
-fold_3 = all_data[int(2*decile):int(3*decile)]
-fold_4 = all_data[int(3*decile):int(4*decile)]
-fold_5 = all_data[int(4*decile):int(5*decile)]
-fold_6 = all_data[int(5*decile):int(6*decile)]
-fold_7 = all_data[int(6*decile):int(7*decile)]
-fold_8 = all_data[int(7*decile):int(8*decile)]
-fold_9 = all_data[int(8*decile):int(9*decile)]
-fold_10 = all_data[int(9*decile):]
