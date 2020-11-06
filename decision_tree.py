e0888d92564b27c1284cb81cfcb6336815e19c5b
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-11-06 17:28:07 +0000

    ok

--- decision_tree.py
+++ decision_tree.py
@@ -456,7 +456,7 @@ def create_plot(root, depth):
 
     print_tree(decision, leaf, arrows, root, root_x, root_y, canvas_w, canvas_h, static_root)
 
-    plt.savefig("treenoisy.png", bbox_inches="tight")
+    plt.savefig("treeclean.png", bbox_inches="tight")
 
 def create_plot2(root, depth):
 
@@ -482,11 +482,11 @@ def create_plot2(root, depth):
 
     print_tree(decision, leaf, arrows, root, root_x, root_y, canvas_w, canvas_h, static_root)
 
-    plt.savefig("prunedtreenoisy.png", bbox_inches="tight")
+    plt.savefig("prunedtreeclean.png", bbox_inches="tight")
 
 def main():
 
-    all_data = np.loadtxt("WIFI.db/noisy_dataset.txt")
+    all_data = np.loadtxt("WIFI.db/clean_dataset.txt")
 
     shuffle(all_data)
 
