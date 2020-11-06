dae8edf63d1e24758bfcf387956b33582e413dac
George Chen <56446026+ghchen99@users.noreply.github.com>
2020-11-06 17:25:34 +0000

    ok

--- decision_tree.py
+++ decision_tree.py
@@ -456,8 +456,33 @@ def create_plot(root, depth):
 
     print_tree(decision, leaf, arrows, root, root_x, root_y, canvas_w, canvas_h, static_root)
 
-    plt.savefig("prunedtreenoisy.png", bbox_inches="tight")
+    plt.savefig("treenoisy.png", bbox_inches="tight")
 
+def create_plot2(root, depth):
+
+    # Decorations
+    decision = dict(boxstyle="round", edgecolor="#023e8a", facecolor="white", )
+    leaf = dict(boxstyle="round", facecolor="0.8")
+    arrows = dict(arrowstyle="<-", color="#9d0208")
+
+    fig = plt.figure(1, facecolor="white")
+    fig.clf()
+    #remove grid
+    axprops = dict(xticks=[], yticks=[])
+    plt.subplot(111, frameon=False, **axprops)
+
+    canvas_w = float(0.04*num_leaves(root))
+    canvas_h = float(depth)
+    print_tree.x_val=0.5/canvas_w
+    print_tree.y_val=1.0
+    root_x = 0.5
+    root_y = 1.0
+
+    static_root = root
+
+    print_tree(decision, leaf, arrows, root, root_x, root_y, canvas_w, canvas_h, static_root)
+
+    plt.savefig("prunedtreenoisy.png", bbox_inches="tight")
 
 def main():
 
@@ -473,12 +498,13 @@ def main():
     node, depth = decision_tree_learning(training, 0)
 
     print("-----PRINT TREE------")
+    create_plot(node, depth)
 
     # print(node)
     print("Number of Leaves: ", num_leaves(node))
 
     node, depth = depth_search(node, training, validation, node, 0)
-    create_plot(node, depth)
+    create_plot2(node, depth)
 
     #evaluate(all_data, node)
 
